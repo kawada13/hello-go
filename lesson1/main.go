522d320b9d776167e75c2ac49560777cda3e7744
kawada <vgnsl94@gmail.com>
2022-01-21 01:34:46 +0900

    s

--- lesson1/main.go
+++ lesson1/main.go
@@ -6,27 +6,10 @@ import (
 
 func main() {
 	var i int = 100
-	fmt.Println(i)
+	var i32 int32 = int32(i)
+	fmt.Println(i, i32)
 
-	var s string = "konnnitiha"
-	fmt.Println(s)
+	fmt.Printf("%T\n", i)
+	fmt.Printf("%T\n", i32)
 
-	var t, f bool = true, false
-	fmt.Println(t, f)
-
-	var (
-		i2 int    = 200
-		s2 string = "hyde"
-	)
-
-	fmt.Println(i2, s2)
-
-	// 暗黙的な定義
-	// 最初に定義した型で決まる
-	// 関数内でしか定義できない⇨明示的な定義は関数外で定義できる
-	// 明示的な定義と暗黙的な定義の違いは、関数内か関数外で定義するかで決める
-	// 一般的には明示的な定義を使うようにする
-	// 変数は定義したら必ずその変数を使わないとエラーが出る
-	i4 := 450
-	fmt.Println(i4)
 }
